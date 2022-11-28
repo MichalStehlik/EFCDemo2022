f8730ca9f7f1ebed19d4553cf9331d575369a5a0
MichalStehlik <michal.stehlik@pslib.cz>
2022-11-28 10:07:49 +0100

    query, explicit loading

---
+++ EFCDemo2022/Models/CompanyListIM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCDemo2022.Models
+{
+    internal class CompanyListIM
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public int GameCount { get; set; }
+    }
+}
--- EFCDemo2022/Program.cs
+++ EFCDemo2022/Program.cs
@@ -1,7 +1,46 @@
 ﻿using EFCDemo2022.Data;
+using EFCDemo2022.Models;
+using Microsoft.EntityFrameworkCore;
 
 var db = new ApplicationDbContext(@"Data Source=myGamesDatabase.sqlite");
+
 foreach(var c in db.Companies.ToList())
 {
     Console.WriteLine(c.Name);
+}
+foreach (var g in db.Games.Include(g => g.Developer).ToList())
+{
+    Console.WriteLine(g.Name + " - " + g.Developer?.Name);
+}
+
+foreach (var c in db.Companies
+    .Include(c => c.Games)
+    .Select(c => new CompanyListIM{ CompanyId = c.CompanyId, Name = c.Name, GameCount = c.Games.Count}).ToList())
+{
+    Console.WriteLine(c.Name + " - " + c.GameCount);
+}
+
+IQueryable<Game> games = db.Games;
+games = games.Where(g => g.DeveloperId == 1);
+games = games.OrderBy(g => g.Name);
+games.ToList();
+foreach(Game g in games)
+{
+    Console.WriteLine(g.Name);
+}
+
+Game game = db.Games.FirstOrDefault(g => g.GameId == 1);
+if (game != null)
+{
+    db.Entry(game).Reference(g => g.Developer).Load();
+    Console.WriteLine(game.Name + " - " + game.Developer?.Name);
+}
+
+Company company = db.Companies.SingleOrDefault(c => c.CompanyId == 1);
+if (company != null)
+{
+    db.Entry(company).Collection(c => c.Games)
+        .Query().Where(g => g.Name.Length > 5)
+        .ToList();
+    Console.WriteLine(company.Name);
 }
